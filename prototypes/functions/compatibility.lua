d78e9719d26e5a6fc77c9d33dbf7e41a91d9d35c
ShadowGlass0 <gkecskem@gmail.com>
2022-07-27 02:29:37 +1000

    resolves pyanodon/pybugreports#38

--- prototypes/functions/compatibility.lua
+++ prototypes/functions/compatibility.lua
@@ -162,3 +162,13 @@ if mods["miniloader"] then
     TECHNOLOGY("fast-miniloader"):add_pack("py-science-pack-2")
     RECIPE("chute-miniloader"):add_ingredient{"burner-inserter", 2}
 end
+
+if mods["Flare Stack"] then
+    local cat = table.array_to_dictionary({"gas-venting", "flaring", "incineration", "fuel-incineration"}, true)
+
+    for recipe_name, recipe in pairs(data.raw.recipe) do
+        if cat[recipe.category] then
+            data.raw.recipe[recipe_name].ignore_for_dependencies = true
+        end
+    end
+end
