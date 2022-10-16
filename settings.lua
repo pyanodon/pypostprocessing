84767314c3d3786753cf246e6379dafaa88f1d5d
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-10-15 22:48:40 -0600

    Add setting for verbose logging

---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "pypp-verboselog",
+        order = "pypp-verboselog",
+        setting_type = "startup",
+        default_value = false
+    }
+})
