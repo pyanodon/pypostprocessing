149155ed56fdacd13a315c053b7a63098baee1bd
oorzkws <65210810+oorzkws@users.noreply.github.com>
2022-10-15 22:47:17 -0600

    Compat: Subspace Storage/Clusterio
    Resolves #169

--- prototypes/functions/compatibility.lua
+++ prototypes/functions/compatibility.lua
@@ -349,6 +349,25 @@ if mods["pushbutton"] then
     RECIPE("pushbutton"):remove_ingredient("advanced-circuit")
 end
 
+if mods["subspace_storage"] then
+    for item_name, item_data in pairs(data.raw.item) do
+        if item_data.subgroup == "subspace_storage-interactor" then
+            item_data.ignore_for_dependencies = true
+            if data.raw.recipe[item_name] then
+                data.raw.recipe[item_name].ignore_for_dependencies = true
+            end
+        end
+    end
+    -- "get-n" is a pretty generic pattern to rely on, so we also make sure the results are empty.
+    for fluid_name in pairs(data.raw.fluid) do
+        local fluid_recipe = data.raw.recipe["get-" .. fluid_name]
+        local fluid_result = fluid_recipe and fluid_recipe.results
+        if fluid_result and fluid_result[1] and fluid_result[1].amount == 0 then
+            fluid_recipe.ignore_for_dependencies = true
+        end
+    end
+end
+
 if data.raw.recipe["electronic-circuit"].enabled == false
     and (not data.raw.recipe["electronic-circuit-initial"] or data.raw.recipe["electronic-circuit-initial"].enabled == false)
     and data.raw.recipe["inductor1-2"]
