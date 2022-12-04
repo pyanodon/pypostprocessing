559fc93e68b016594f2094171819a74e5e20da58
kingarthur91 <arthurkoy_durbin@yahoo.com>
2022-12-03 22:53:45 -0500

    Update compatibility.lua
    resolves pyanodon/pybugreports#199

--- prototypes/functions/compatibility.lua
+++ prototypes/functions/compatibility.lua
@@ -67,6 +67,11 @@ end
 
 if mods["LightedPolesPlus"] then
     RECIPE("lighted-small-electric-pole"):add_unlock("optics"):remove_unlock("creosote"):set_enabled(false)
+    if mods["pyalternativeenergy"] then
+        RECIPE("lighted-medium-electric-pole"):remove_unlock("optics"):add_unlock("electric-energy-distribution-1")
+        RECIPE("lighted-big-electric-pole"):remove_unlock("optics"):add_unlock("electric-energy-distribution-2")
+        RECIPE("lighted-substation"):remove_unlock("optics"):add_unlock("electric-energy-distribution-4")
+    end
 end
 
 if mods["reverse-factory"] then
