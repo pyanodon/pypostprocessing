d38efd1d729ad80793bc7086ff85c5a3cc50f9f4
ShadowGlass0 <gkecskem@gmail.com>
2023-01-30 10:02:15 +1100

    Change verbose logging to hidden setting

--- settings-updates.lua
+++ settings-updates.lua
@@ -1,2 +1,3 @@
 data.raw["bool-setting"]["pypp-dev-mode"].forced_value  = false
 data.raw["bool-setting"]["pypp-create-cache"].forced_value  = false
+data.raw["bool-setting"]["pypp-verbose-logging"].forced_value  = false
--- settings.lua
+++ settings.lua
@@ -1,27 +1,27 @@
 data:extend(
 {
-	{
+    {
         type = "bool-setting",
         name = "pypp-dev-mode",
         setting_type = "startup",
         default_value = false,
         hidden = true,
         order = "e",
-	},
-	{
+    },
+    {
         type = "bool-setting",
         name = "pypp-create-cache",
         setting_type = "startup",
         default_value = false,
         hidden = true,
         order = "e",
-	},
-	{
+    },
+    {
         type = "bool-setting",
         name = "pypp-verbose-logging",
         setting_type = "startup",
         default_value = false,
-        hidden = false,
+        hidden = true,
         order = "e",
-	},
+    },
 })
