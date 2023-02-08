6d69d77eb79d236a9935b3c0d8e5eaa8b968c436
oorzkws <65210810+oorzkws@users.noreply.github.com>
2023-02-08 14:14:32 -0700

    Let silly players to fix their saves

--- control.lua
+++ control.lua
@@ -7,3 +7,43 @@ script.on_configuration_changed(function()
 	   force.reset_technology_effects()
 	end
 end)
+
+
+-- We risk more problems than solutions if we expand further
+-- This will need changes if TURD works off locking techs!
+local checked_mods = {
+	base = true,
+	pyalienlife = true,
+	pyalternativeenergy = true,
+	pycoalprocessing = true,
+	pyfusionenergy = true,
+	pyhightech = true,
+	pyindustry = true,
+	pypetroleumhandling = true,
+	pypostprocessing = true,
+	pyrawores = true
+}
+
+commands.add_command('check-technology-consistency', {'command-help.check-technology-consistency'}, function()
+	local prototypes = {}
+	-- Build a list of base-game techs
+	for name, prototype in pairs(game.technology_prototypes) do
+		local history = script.get_prototype_history('technology', name)
+		if checked_mods[history.created] then
+			prototypes[name] = prototype
+		end
+	end
+	-- Iterate and verify
+	for _, force in pairs(game.forces) do
+		local force_techs = force.technologies
+		for name, prototype in pairs(prototypes) do
+			local tech = force_techs[name]
+			if tech.enabled ~= prototype.enabled then
+				tech.enabled = prototype.enabled
+				local localised_name = tech.localised_name or ('technology-name.' .. name)
+				game.print({'command-output.fixed-technology', localised_name})
+			end
+		end
+	end
+	game.print({'command-output.consistency-check-complete'})
+end)
