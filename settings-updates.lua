c5ca80de29c5ae0badfd394c17ba68a5b1b1ce35
kingarthur91 <arthurkoy_durbin@yahoo.com>
2023-04-16 04:24:57 -0400

    update cache again after fixing pycp py sci pack error

--- settings-updates.lua
+++ settings-updates.lua
@@ -1,3 +1,4 @@
-data.raw["bool-setting"]["pypp-dev-mode"].forced_value  = false
+﻿data.raw["bool-setting"]["pypp-dev-mode"].forced_value  = false
 data.raw["bool-setting"]["pypp-create-cache"].forced_value  = false
 data.raw["bool-setting"]["pypp-verbose-logging"].forced_value  = false
+
