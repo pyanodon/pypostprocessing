03227fc7f9a7653113ab94d5e6649f51619cd8a8
notnotmelon <zacharypicco@gmail.com>
2023-07-14 12:58:40 -0500

    improve ALT-mode icon size for the largest machines.

--- data-updates.lua
+++ data-updates.lua
@@ -34,3 +34,23 @@ set_underground_recipe("underground-belt", "transport-belt", nil, nil)
 set_underground_recipe("fast-underground-belt", "fast-transport-belt", "underground-belt", "transport-belt")
 set_underground_recipe("express-underground-belt", "express-transport-belt", "fast-underground-belt", "fast-transport-belt")
 
+local big_recipe_icons_blacklist = {
+    ["rc-mk01"] = true,
+    ["rc-mk02"] = true,
+    ["rc-mk03"] = true,
+    ["rc-mk04"] = true,
+}
+
+for _, prototype in pairs{"assembling-machine", "furnace"} do
+    for _, entity in pairs(data.raw[prototype]) do
+        if not entity.name or big_recipe_icons_blacklist[entity.name] then goto continue end
+        local box = entity.selection_box or entity.collision_box
+        if not box or not box[1] or not box[2] then goto continue end
+        local x, xx, y, yy = box[1][1], box[2][1], box[1][2], box[2][2]
+        if not x or not xx or not y or not yy then goto continue end
+        local area = (xx - x) * (yy - y)
+        if area <= 9 then goto continue end
+        entity.scale_entity_info_icon = true
+        ::continue::
+    end
+end
