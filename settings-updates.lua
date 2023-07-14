bcd5cf588c4842be09fbb4d4eed88317cd0ec2a2
notnotmelon <zacharypicco@gmail.com>
2023-07-14 08:27:36 -0500

    regenerate

--- settings-updates.lua
+++ settings-updates.lua
@@ -1,6 +1,7 @@
-﻿data.raw["bool-setting"]["pypp-dev-mode"].forced_value  = false
-data.raw["bool-setting"]["pypp-create-cache"].forced_value  = false
+﻿data.raw["bool-setting"]["pypp-dev-mode"].forced_value  = true
+data.raw["bool-setting"]["pypp-create-cache"].forced_value  = true
 data.raw["bool-setting"]["pypp-verbose-logging"].forced_value  = false
+
 
 
 
