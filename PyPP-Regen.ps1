395dc7b93dfe13fbd1030877ff1d314dee467a5f
Alex ten Brink <alextenbrink@gmail.com>
2023-08-26 14:12:25 +0200

    Made PyPP-Regen.ps1 more robust: - now pauses on error, so you can read the error it prints - Factorio is now run in the same command line, to facilitate the above - on control+C interrupt, now still restores your settings-updates.lua - now works for Factorio.exe paths with spaces in it - now enforces CRLF line endings

--- PyPP-Regen.ps1
+++ PyPP-Regen.ps1
@@ -3,7 +3,7 @@
     [String]$FactorioDataPath = "$env:APPDATA\Factorio",
     [String]$FactorioModsPath = "$FactorioDataPath\mods.dev"
 )
-$FactorioArgs = "$FactorioPath --mod-directory $FactorioModsPath --benchmark notafile" #Stand in for "load then exit"
+$FactorioArgs = "--mod-directory $FactorioModsPath --benchmark notafile" #Stand in for "load then exit"
 $ModCombinations = (Get-ChildItem "$FactorioModsPath\pypostprocessing\cached-configs" -Exclude run.lua).BaseName
 $StartToken = "<BEGINPYPP>" #\1
 $EndToken = "<ENDPYPP>" #\2
@@ -12,6 +12,10 @@ $PyPPConfigPath = "$FactorioModsPath\pypostprocessing\settings-updates.lua"
 $PyPPPrevConfig = Get-Content -Path $PyPPConfigPath -Raw
 $PyPPConfig = $PyPPPrevConfig.Replace('data.raw["bool-setting"]["pypp-dev-mode"].forced_value  = false', 'data.raw["bool-setting"]["pypp-dev-mode"].forced_value  = true')
 $PyPPConfig = $PyPPConfig.Replace('data.raw["bool-setting"]["pypp-create-cache"].forced_value  = false', 'data.raw["bool-setting"]["pypp-create-cache"].forced_value  = true')
+
+#this restores the settings even if someone control+C kills the script. Won't work if you exit the containing shell though...
+Register-EngineEvent PowerShell.Exiting –Action { Set-Content -Path $PyPPConfigPath -Value $PyPPPrevConfig -Encoding UTF8 -NoNewline }
+
 Set-Content -Path $PyPPConfigPath -Value $PyPPConfig -Encoding UTF8 -NoNewline
 $ModListJson = "$FactorioModsPath\mod-list.json"
 ForEach($ModCombination in $ModCombinations){
@@ -42,19 +46,20 @@ ForEach($ModCombination in $ModCombinations){
     #Set-Content -Path (Split-Path -Path $FactorioPath -Parent) -Value "427520" -Encoding UTF8 -NoNewline -ErrorAction SilentlyContinue
     Set-Content -Path "$FactorioModsPath\mod-list.json" -Value $ModListContent -Encoding UTF8 -NoNewline
     Write-Output "Generating for mod set $ModCombination"
-    Start-Process "cmd" -ArgumentList "/c start `"`" $FactorioPath $FactorioArgs" -WorkingDirectory (Split-Path -Path $FactorioPath -Parent) -Wait
+    Start-Process -FilePath "$FactorioPath" -ArgumentList "$FactorioArgs" -WorkingDirectory (Split-Path -Path $FactorioPath -Parent) -Wait
     $LogFile = Get-Content -Path "$FactorioDataPath\factorio-current.log" -Raw
     $StartPos = $LogFile.IndexOf($StartToken)
     if($StartPos -gt 0){
         $StartPos += $StartToken.Length
         $SubsectionLength = ($LogFile.IndexOf($EndToken) - $StartPos)
         $Content = $LogFile.Substring($StartPos, $SubsectionLength)
-        Set-Content -Path "$FactorioModsPath\pypostprocessing\cached-configs\$ModCombination.lua" -Value $Content -Encoding UTF8 -NoNewline
+        $Content = $Content -replace "([^`r])`n", "`$1`r`n" -replace "^`n", "`r`n" #enforce CR LF
+        $CachePath = "$FactorioModsPath\pypostprocessing\cached-configs\$ModCombination.lua"
+        Set-Content -Path $CachePath -Value $Content -Encoding UTF8 -NoNewline
         Write-Output "Finished generating for mod set $ModCombination"
     }else{
-        Write-Warning -Message "Mod Set $ModCombination did not load successfully"
+        Read-Host -Prompt "Mod Set $ModCombination did not load successfully. Pausing for dramatic effect and so you can read the error message."
     }
 }
-Set-Content -Path $PyPPConfigPath -Value $PyPPPrevConfig -Encoding UTF8 -NoNewline
 $PyPPPrevConfig | Out-File -FilePath $PyPPConfigPath -Encoding utf8
 pause
