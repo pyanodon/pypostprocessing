c75b67099c0ef351691d51aef413aacf58b74094
Alex ten Brink <alextenbrink@gmail.com>
2023-10-02 23:12:09 +0200

    Rewrote cache file registration.
    It now has the following features:
    - mods can register a cache file and a set of mods it applies to
    - PyPP registers some fallback cache files that are ignored if there's a custom cache
    - instead of a hardcoded list of Py mods, we look at the union of all mods mentioned by a cache file
    - we require a cache file whose registered set of mods exactly matches the enabled mentioned mods

--- cached-configs/run.lua
+++ cached-configs/run.lua
@@ -1,4 +1,4 @@
-local config = require 'prototypes.config'
+local Table = require('__stdlib__/stdlib/utils/table')
 
 local function merge(table, value)
 	for k, v in pairs(value) do
@@ -26,61 +26,87 @@ function _G.science_pack_order(science_pack, order)
 	sp.order = order
 end
 
-local success = false
-local function run_cache_files(subset)
-	local truth_table = {}
-	for _, mod in pairs(subset) do truth_table[mod] = true end
-	
-	for _, pymod in pairs(config.PYMODS) do
-		if (not truth_table[pymod]) ~= (not mods[pymod]) then
-			-- the current modlist does match input
-			return
-		end
-	end
-	
-	if success then error('pypostprocessing cache files were ran twice! Please report this') end
-	success = true
-
+local function register_cache_file_pypp(subset)
 	table.sort(subset)
-	subset = table.concat(subset, '+')
-	log('running cached config ' .. subset)
-	require(subset)
-end
-
-if pypp_cache_file_to_use ~= nil then
-	--pypp_cache_file_to_use()
-	--return
+	local cache_file = table.concat(subset, '+')
+	pypp_registered_cache_files[#pypp_registered_cache_files + 1] =
+	{
+		subset=subset,
+		cache_file=cache_file,
+		is_fallback_from_pypp = true,
+	}
 end
 
 -- simple py
-run_cache_files{'pycoalprocessing'}
-run_cache_files{'pyindustry'}
-run_cache_files{'pyindustry', 'pycoalprocessing'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry'}
+register_cache_file_pypp{'pycoalprocessing'}
+register_cache_file_pypp{'pyindustry'}
+register_cache_file_pypp{'pyindustry', 'pycoalprocessing'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry'}
 
 -- medium py
-run_cache_files{'pycoalprocessing', 'pyindustry', 'pyrawores'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pypetroleumhandling'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyhightech'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pyhightech'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pypetroleumhandling', 'pyhightech'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyhightech'}
+register_cache_file_pypp{'pycoalprocessing', 'pyindustry', 'pyrawores'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pypetroleumhandling'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyhightech'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pyhightech'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pypetroleumhandling', 'pyhightech'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyhightech'}
 
 -- full py
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife', 'pyhightech'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife', 'pyhightech', 'pyalternativeenergy'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife', 'pyhightech'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife', 'pyhightech', 'pyalternativeenergy'}
 
 -- PyBlock
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife', 'pyhightech', 'pyalternativeenergy', 'PyBlock'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife', 'pyhightech', 'pyalternativeenergy', 'PyBlock'}
 
 -- space py
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife', 'pyhightech', 'pyalternativeenergy', 'pystellarexpedition'}
-run_cache_files{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife', 'pyhightech', 'pyalternativeenergy', 'pystellarexpedition', 'pyaliens'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife', 'pyhightech', 'pyalternativeenergy', 'pystellarexpedition'}
+register_cache_file_pypp{'pycoalprocessing', 'pyfusionenergy', 'pyindustry', 'pyrawores', 'pypetroleumhandling', 'pyalienlife', 'pyhightech', 'pyalternativeenergy', 'pystellarexpedition', 'pyaliens'}
 
-if not success then
-	error('pypostprocessing was not loaded correctly! Please report this')
+local union_of_all_subsets = {}
+for _, cache_file_info in pairs(pypp_registered_cache_files) do
+	for _, mod in pairs(cache_file_info.subset) do
+		union_of_all_subsets[mod] = 1
+	end
+end
+union_of_all_subsets = Table.keys(union_of_all_subsets)
+local recognized_enabled_mods = Table.filter(union_of_all_subsets, function(potential_mod) return mods[potential_mod] end)
+table.sort(recognized_enabled_mods)
+
+local function can_left_replace_fallback_right(left, right)
+	return not left.is_fallback_from_pypp and right.is_fallback_from_pypp
+end
+
+local best_cache_file = nil
+for _, cache_file_info in pairs(pypp_registered_cache_files) do
+	local subset = cache_file_info.subset
+	if #subset == #recognized_enabled_mods then
+		local is_applicable = true
+		for i = 1, #recognized_enabled_mods do
+			if subset[i] ~= recognized_enabled_mods[i] then
+				is_applicable = false
+				break
+			end
+		end
+		if is_applicable then
+			if best_cache_file == nil or can_left_replace_fallback_right(cache_file_info, best_cache_file) then
+				best_cache_file = cache_file_info
+			elseif not can_left_replace_fallback_right(best_cache_file, cache_file_info) then
+				error("There are two cache files for mod sets registered that are equally good given the mods that are active. The first is " .. best_cache_file.cache_file .. ", the second is " .. cache_file_info.cache_file)
+			end
+		end
+	end
+end
+
+if best_cache_file == nil then
+	for _, cache_file_info in pairs(pypp_registered_cache_files) do
+		log("Cache file " .. cache_file_info.cache_file .. ", supported mods " .. table.concat(cache_file_info.subset, "+"))
+	end
+	error("No cache file registered that supports all your enabled mods. Enabled known mods: " .. table.concat(recognized_enabled_mods, "+") .. ", registered cache files printed in logs.")
+else
+	log("Unique best cache file found, applying " .. best_cache_file.cache_file .. ", which " .. (best_cache_file.is_fallback_from_pypp and "is" or "is not") .. " a PyPP default.")
+	require(best_cache_file.cache_file)
 end
---
+++ data.lua
@@ -0,0 +1,13 @@
+pypp_registered_cache_files = {}
+
+-- Usage example:
+-- register_cache_file({"pycoalprocessing", "pyfusionenergy"}, "__pyfusionenergy__/cached-configs/pycoalprocessing+pyfusionenergy.lua")
+function _G.register_cache_file(subset, cache_file)
+	table.sort(subset)
+	pypp_registered_cache_files[#pypp_registered_cache_files+1] =
+	{
+		subset=subset,
+		cache_file=cache_file,
+		is_fallback_from_pypp = false,
+	}
+end
