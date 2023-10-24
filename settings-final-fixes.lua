1fd1cbf0b2635f8bf81df91547c5b87d72e1118e
Erkigmo <122825731+Erkigmo@users.noreply.github.com>
2023-10-23 20:55:23 -0400

    Transport Drones Compat

--- settings-final-fixes.lua
+++ settings-final-fixes.lua
@@ -3,3 +3,11 @@ if mods["alien-biomes"] and mods["pycoalprocessing"] and data.raw["string-settin
     data.raw["string-setting"]["alien-biomes-disable-vegetation"].allowed_values = { "Disabled" }
     data.raw["string-setting"]["alien-biomes-disable-vegetation"].hidden = true
 end
+
+if mods['Transport_Drones'] then
+    data.raw["double-setting"]["fuel-consumption-per-meter"].default_value = 0.01
+    data.raw["double-setting"]["fuel-amount-per-drone"].default_value = 25
+    if mods['pypetroleumhandling'] then
+        data.raw['string-setting']['fuel-fluid'].default_value = "gasoline"
+    end
+end
