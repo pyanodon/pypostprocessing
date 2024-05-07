2a55ccb14ba4404d09a097605bdc8c3b33a36c66
oorzkws <65210810+oorzkws@users.noreply.github.com>
2024-05-06 20:32:48 -0600

    Skip prod blacklist for single-result item recipes

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -53,14 +53,17 @@ for _, recipe in pairs(data.raw.recipe) do
             recipe.result = nil
             recipe.result_count = nil
         end
-        for i, result in pairs(recipe.results) do
-            local name = result.name or result[1]
-            local amount = result.amount or result[2]
-            if name and config.NON_PRODDABLE_ITEMS[name] and not result.catalyst_amount then
-                if result[1] then
-                    recipe.results[i] = {type = result.type or 'item', name = name, amount = amount, catalyst_amount = amount}
-                else
-                    result.catalyst_amount = amount
+        -- Skip if recipe only produces the item, not uses it as a catalyst. Fluids are probably blacklisted for other reasons.
+        if #recipe.results ~= 1 or recipe.results[1].type ~= 'fluid' then
+            for i, result in pairs(recipe.results) do
+                local name = result.name or result[1]
+                local amount = result.amount or result[2]
+                if name and config.NON_PRODDABLE_ITEMS[name] and not result.catalyst_amount then
+                    if result[1] then
+                        recipe.results[i] = {type = result.type or 'item', name = name, amount = amount, catalyst_amount = amount, [1] = nil, [2] = nil}
+                    else
+                        result.catalyst_amount = amount
+                    end
                 end
             end
         end
