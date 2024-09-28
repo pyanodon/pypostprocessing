2ef2d0b7f11d70a6f39525630de602df6f3615d9
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-28 14:50:29 +0200

    more hr_version bullshit

--- lib/data-stage.lua
+++ lib/data-stage.lua
@@ -241,30 +241,17 @@ py.pipe_covers = function(n, s, w, e)
                 {
                     filename = '__base__/graphics/entity/pipe-covers/pipe-cover-north.png',
                     priority = 'extra-high',
-                    width = 64,
-                    height = 64,
-                    hr_version = {
-                        filename = '__base__/graphics/entity/pipe-covers/hr-pipe-cover-north.png',
-                        priority = 'extra-high',
-                        width = 128,
-                        height = 128,
-                        scale = 0.5
-                    }
+                    width = 128,
+                    height = 128,
+                    scale = 0.5
                 },
                 {
                     filename = '__base__/graphics/entity/pipe-covers/pipe-cover-north-shadow.png',
                     priority = 'extra-high',
-                    width = 64,
-                    height = 64,
-                    draw_as_shadow = true,
-                    hr_version = {
-                        filename = '__base__/graphics/entity/pipe-covers/hr-pipe-cover-north-shadow.png',
-                        priority = 'extra-high',
-                        width = 128,
-                        height = 128,
-                        scale = 0.5,
-                        draw_as_shadow = true
-                    }
+                    width = 128,
+                    height = 128,
+                    scale = 0.5,
+                    draw_as_shadow = true
                 }
             }
         } or
@@ -276,30 +263,17 @@ py.pipe_covers = function(n, s, w, e)
                 {
                     filename = '__base__/graphics/entity/pipe-covers/pipe-cover-east.png',
                     priority = 'extra-high',
-                    width = 64,
-                    height = 64,
-                    hr_version = {
-                        filename = '__base__/graphics/entity/pipe-covers/hr-pipe-cover-east.png',
-                        priority = 'extra-high',
-                        width = 128,
-                        height = 128,
-                        scale = 0.5
-                    }
+                    width = 128,
+                    height = 128,
+                    scale = 0.5
                 },
                 {
                     filename = '__base__/graphics/entity/pipe-covers/pipe-cover-east-shadow.png',
                     priority = 'extra-high',
-                    width = 64,
-                    height = 64,
-                    draw_as_shadow = true,
-                    hr_version = {
-                        filename = '__base__/graphics/entity/pipe-covers/hr-pipe-cover-east-shadow.png',
-                        priority = 'extra-high',
-                        width = 128,
-                        height = 128,
-                        scale = 0.5,
-                        draw_as_shadow = true
-                    }
+                    width = 128,
+                    height = 128,
+                    scale = 0.5,
+                    draw_as_shadow = true
                 }
             }
         } or
@@ -311,30 +285,17 @@ py.pipe_covers = function(n, s, w, e)
                 {
                     filename = '__base__/graphics/entity/pipe-covers/pipe-cover-south.png',
                     priority = 'extra-high',
-                    width = 64,
-                    height = 64,
-                    hr_version = {
-                        filename = '__base__/graphics/entity/pipe-covers/hr-pipe-cover-south.png',
-                        priority = 'extra-high',
-                        width = 128,
-                        height = 128,
-                        scale = 0.5
-                    }
+                    width = 128,
+                    height = 128,
+                    scale = 0.5
                 },
                 {
                     filename = '__base__/graphics/entity/pipe-covers/pipe-cover-south-shadow.png',
                     priority = 'extra-high',
-                    width = 64,
-                    height = 64,
-                    draw_as_shadow = true,
-                    hr_version = {
-                        filename = '__base__/graphics/entity/pipe-covers/hr-pipe-cover-south-shadow.png',
-                        priority = 'extra-high',
-                        width = 128,
-                        height = 128,
-                        scale = 0.5,
-                        draw_as_shadow = true
-                    }
+                    width = 128,
+                    height = 128,
+                    scale = 0.5,
+                    draw_as_shadow = true
                 }
             }
         } or
@@ -346,30 +307,17 @@ py.pipe_covers = function(n, s, w, e)
                 {
                     filename = '__base__/graphics/entity/pipe-covers/pipe-cover-west.png',
                     priority = 'extra-high',
-                    width = 64,
-                    height = 64,
-                    hr_version = {
-                        filename = '__base__/graphics/entity/pipe-covers/hr-pipe-cover-west.png',
-                        priority = 'extra-high',
-                        width = 128,
-                        height = 128,
-                        scale = 0.5
-                    }
+                    width = 128,
+                    height = 128,
+                    scale = 0.5
                 },
                 {
                     filename = '__base__/graphics/entity/pipe-covers/pipe-cover-west-shadow.png',
                     priority = 'extra-high',
-                    width = 64,
-                    height = 64,
-                    draw_as_shadow = true,
-                    hr_version = {
-                        filename = '__base__/graphics/entity/pipe-covers/hr-pipe-cover-west-shadow.png',
-                        priority = 'extra-high',
-                        width = 128,
-                        height = 128,
-                        scale = 0.5,
-                        draw_as_shadow = true
-                    }
+                    width = 128,
+                    height = 128,
+                    scale = 0.5,
+                    draw_as_shadow = true
                 }
             }
         } or
