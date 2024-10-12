bc601423f6ee46874974442732f88803c72a7c1b
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-10-12 20:36:49 +0200

    fix locale test

--- tests/control.lua
+++ tests/control.lua
@@ -2,7 +2,7 @@ local function test_localised_strings()
     local excluded_categories = {}
     local localised_strings = {}
     for _, recipe in pairs(prototypes.recipe) do
-        if not excluded_categories[recipe.category] then table.insert(localised_strings, recipe.localised_name) end
+        if not excluded_categories[recipe.category] and not recipe.hidden then table.insert(localised_strings, recipe.localised_name) end
     end
     game.players[1].request_translations(localised_strings)
 end
