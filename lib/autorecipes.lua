191bb41452359fc8d8b211f62da4343bfa9cbe47
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-10-18 19:26:35 +0200

    allow prod in autorecipes

--- lib/autorecipes.lua
+++ lib/autorecipes.lua
@@ -265,7 +265,8 @@ py.autorecipes = function(params)
             order = params.order,
             allowed_module_categories = params.allowed_module_categories,
             icons = tier.icons,
-            main_product = tier.main_product or params.main_product
+            main_product = tier.main_product or params.main_product,
+            allow_productivity = true,
         }
         if tier.tech then recipe:add_unlock(tier.tech) end
 
