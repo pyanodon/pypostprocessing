2192061a625d4027970e670bfdedb0b9c1d89f2d
notnotmelon <zacharypicco@gmail.com>
2024-11-03 20:59:26 -0600

    Fixed quality compat being a bit over-eager and deleting any recipes with "recycling" in the name.

--- prototypes/quality.lua
+++ prototypes/quality.lua
@@ -1,8 +1,10 @@
 if not mods.quality then return end
 
+local RECYCLING_ICON_FILEPATH = "__quality__/graphics/icons/recycling.png"
+
 local deadrecipes = {}
 for r, recipe in pairs(data.raw.recipe) do
-    if string.match(recipe.name, "recycling") ~= nil then
+    if string.match(recipe.name, "recycling") and recipe.icons and recipe.icons[1] and recipe.icons[1].icon == RECYCLING_ICON_FILEPATH then
         if data.raw.item[recipe.ingredients[1].name] == nil or data.raw.item[recipe.results[1].name] == nil then
             table.insert(deadrecipes, recipe.name)
         end
@@ -12,16 +14,4 @@ end
 for r, recipe in pairs(deadrecipes) do
     log("WARNING @ quality.lua: Deleting quality recycling recipe " .. recipe)
     data.raw.recipe[recipe] = nil
-end
-local deadrecipes = {}
-
-for t, tech in pairs(data.raw.technology) do
-    for e, effect in pairs(tech.effects or {}) do
-        if effect.type == "unlock-recipe" then
-            if data.raw.recipe[effect.recipe] == nil then
-                log("WARNING @ quality.lua " .. tech.name .. ": Recipe unlock effect " .. effect.recipe .. " does not exist!")
-                data.raw.technology[tech.name].effects[e] = nil
-            end
-        end
-    end
 end
