9f649189750d1b1d7a3b2a8d661f0131804ce9bb
oorzkws <65210810+oorzkws@users.noreply.github.com>
2024-11-21 11:31:50 -0700

    Don't assume minable .results has entries
    Resolves pyanodon/pybugreports#739

--- prototypes/next-upgrades.lua
+++ prototypes/next-upgrades.lua
@@ -28,7 +28,7 @@ local function check_for_valid_minable_properties(entity)
 
     local minable = entity.minable
     if not minable.result and not minable.results then return false end
-    local minable_result = minable.result or minable.results[1].name or minable.results[1][1]
+    local minable_result = minable.result or minable.results[1] and (minable.results[1].name or minable.results[1][1])
     if not minable_result then return false end
     if minable_result ~= entity.name then return false end
 
