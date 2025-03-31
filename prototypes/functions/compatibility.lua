53c911b941e46ebe442c53173a7c4293ee52f851
Zachary Picco <zacharypicco@gmail.com>
2025-03-31 17:36:40 -0500

    Proper fix for compaktcircuit mod compatibility.

--- prototypes/functions/compatibility.lua
+++ prototypes/functions/compatibility.lua
@@ -475,10 +475,17 @@ end
 if mods["compaktcircuit"] then
     RECIPE("compaktcircuit-processor"):remove_ingredient("processing-unit")
     RECIPE("compaktcircuit-processor_1x1"):remove_ingredient("processing-unit")
-    TECHNOLOGY("compaktcircuit-tech").prerequisites = {"py-science-pack-mk02"}
-    TECHNOLOGY("compaktcircuit-tech").unit.ingredients = {
-        mods["pyalienlife"] and {"py-science-pack-2", 1} or {"logistic-science-pack", 1}
-    }
+    if mods["pyalienlife"] then
+        TECHNOLOGY("compaktcircuit-tech").prerequisites = {"py-science-pack-mk02"}
+        TECHNOLOGY("compaktcircuit-tech").unit.ingredients = {
+            {"py-science-pack-2", 1}
+        }
+    else
+        TECHNOLOGY("compaktcircuit-tech").prerequisites = {"logistic-science-pack"}
+        TECHNOLOGY("compaktcircuit-tech").unit.ingredients = {
+            {"logistic-science-pack", 1}
+        }
+    end
 end
 
 if mods["bobmodules"] then
