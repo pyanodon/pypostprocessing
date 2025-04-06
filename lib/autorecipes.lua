a25330936d7154a3cbaa3e052866876afc755770
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2025-04-06 14:23:23 +0200

    actually fix autorecipes

--- lib/autorecipes.lua
+++ lib/autorecipes.lua
@@ -266,7 +266,7 @@ py.autorecipes = function(params)
             allowed_module_categories = params.allowed_module_categories,
             icons = tier.icons,
             main_product = tier.main_product or params.main_product,
-            allow_productivity = true,
+            allow_productivity = params.category ~= "slaughterhouse",
         }
         if tier.tech then recipe:add_unlock(tier.tech) end
 
