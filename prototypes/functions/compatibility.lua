55697b2f2393b281b52fb5ff01dc226954444b56
GeneralTanker <69743165+GeneralTanker@users.noreply.github.com>
2025-05-31 00:36:57 -0500

    Added back old compatibility for Larger lamps

--- prototypes/functions/compatibility.lua
+++ prototypes/functions/compatibility.lua
@@ -13,6 +13,15 @@ if mods["galdocs-manufacturing"] then
     require "prototypes/functions/galdoc"
 end
 
+if mods["DeadlockLargerLamp"] then
+    -- Originally these include electronic-circuits and are unlocked at optics, causing a deadlock in pymods
+    RECIPE("deadlock-large-lamp"):remove_ingredient("electronic-circuit"):add_ingredient {type = "item", name = "copper-plate", amount = 4}:add_ingredient {type = "item", name = "glass", amount = 6}
+    RECIPE("deadlock-floor-lamp"):remove_ingredient("electronic-circuit"):add_ingredient {type = "item", name = "copper-plate", amount = 4}:add_ingredient {type = "item", name = "glass", amount = 6}
+
+    data.raw["assembling-machine"]["deadlock-copper-lamp"].energy_source.fuel_categories = {"chemical","biomass","nuke"}
+
+end
+
 if mods["LargerLamps-2_0"] then
     -- Originally these include electronic-circuits and are unlocked at optics, causing a deadlock in pymods
     RECIPE("deadlock-large-lamp"):remove_ingredient("electronic-circuit"):add_ingredient {type = "item", name = "copper-plate", amount = 4}:add_ingredient {type = "item", name = "glass", amount = 6}
