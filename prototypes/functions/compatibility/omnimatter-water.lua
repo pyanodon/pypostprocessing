9aa08c2ad7e20fa890a4d736b8564526cf1b519c
lemonlambda <lemonjameslem@protonmail.com>
2025-06-17 21:34:13 -0400

    Added each compatibility mod to it's own file

---
+++ prototypes/functions/compatibility/omnimatter-water.lua
@@ -0,0 +1,4 @@
+if mods["omnimatter_water"] and mods["pyindustry"] then
+    RECIPE("py-sinkhole"):remove_unlock("steel-processing"):add_unlock("engine")
+end
+
---
+++ prototypes/functions/compatibility/tiny-start.lua
@@ -0,0 +1,4 @@
+if mods["TinyStart"] then
+    data.raw.recipe["tiny-armor-mk1"].ignore_for_dependencies = true
+    data.raw.recipe["tiny-armor-mk2"].ignore_for_dependencies = true
+end
