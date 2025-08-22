f88d8ee544cfa52cda53a4df3491422ffacd08ed
oorzkws <65210810+oorzkws@users.noreply.github.com>
2025-08-21 20:59:03 -0600

    Fix event crash because on_config_changed doesn't get called
    something with requires I guess

--- lib/events.lua
+++ lib/events.lua
@@ -237,6 +237,8 @@ py.on_event(defines.events.on_tick, function(event)
     end
 
     -- gui handler
+    -- do it here too since somehow config_changed doesn't always get called below
+    storage.ignored_players = storage.ignored_players or {}
     for k, expiry in pairs(storage.ignored_players) do
         if tick >= expiry then
             storage.ignored_players[k] = nil
@@ -252,6 +254,7 @@ py.on_event(defines.events.on_tick, function(event)
     storage.on_tick[tick] = nil
 end)
 
+-- somehow this doesn't trigger for on config changed
 py.on_event(py.events.on_init(), function(_)
     storage.ignored_players = storage.ignored_players or {}
 end)
