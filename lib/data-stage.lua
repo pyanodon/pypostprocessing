916e92f260dfe793df2ad976333fe609142bb780
Lynnatic <lynnatic@posteo.de>
2025-09-13 22:07:30 +0200

    Allow py.composite_icon to use pyalienlife plants/creatures
    pyalienlife's plants and creatures are currently created as modules
    instead of items. This causes py.composite_icon to fail, as it only
    searches for fluids/items/recipes and not for modules.
    The modules also specify "icons" instead of "icon", which causes
    another problem when trying to use py.composite_icon with them.
    
    This patch solves these problems by:
    * Reworking the way icons are searched.
    * Adding "modules" to the list of datasets that is searched for icons.
    * Allow the "icons" field to be used (the first icon in the array is
      automatically selected in this case)
    
    Additional changes:
    * Only use a prototype match if it has an "icon" or "icons" field present.
    * Throw a specific "No icon found for prototype X" error message when
      no icon could be found in any of the datasets - Which is a lot easier
      to understand than a rather cryptic "trying to access nil value" error.
    
    Notes:
    * This patch changes only the internal workings of py.composite_icon,
      generated icons will look the same as before.
    * "modules" have been deliberately added after "recipes" to the search list;
      While it would make more sense to search "modules" first, doing so might
      make already existing icons look different, which we want to avoid here.

--- lib/data-stage.lua
+++ lib/data-stage.lua
@@ -157,20 +157,44 @@ function py.composite_icon(base_prototype_string, child_top_left, child_top_righ
     shadow_scale = shadow_scale or 0.6
     child_scale = child_scale or 0.5
     shift = shift or 10
-    local base_prototype = data.raw.fluid[base_prototype_string] or data.raw.item[base_prototype_string] or data.raw.recipe[base_prototype_string]
 
+    local function get_prototype_icon(prototype_string)
+        local keys = { "fluid", "item", "recipe", "module" }
+        
+        for _, key in pairs(keys)
+        do
+            local prototype = data.raw[key][prototype_string]
+            if prototype
+            then
+                if prototype.icon
+                then
+                    return prototype, prototype.icon
+                elseif prototype.icons
+                then
+                    -- the array index starts at 1 instead of 0 in Lua
+                    return prototype, prototype.icons[1].icon
+                end
+            end
+        end
+
+        -- if we reach this part we haven't found an icon to use,
+        -- so we deliberately cause an error to make the problem more clear in the bracktrace
+        error("No icon found for prototype \"" .. prototype_string .. "\"!")
+    end
+
+    local base_prototype, base_prototype_icon = get_prototype_icon(base_prototype_string)
     local icons = {
         {
-            icon = base_prototype.icon,
+            icon = base_prototype_icon,
             icon_size = (base_prototype.icon_size or 64)
         }
     }
 
     -- Add shadow icons
     if child_top_left then
-        local child_prototype = data.raw.fluid[child_top_left] or data.raw.item[child_top_left] or data.raw.recipe[child_top_left]
+        local child_prototype, child_prototype_icon = get_prototype_icon(child_top_left)
         table.insert(icons, {
-            icon = child_prototype.icon,
+            icon = child_prototype_icon,
             icon_size = (child_prototype.icon_size or 64),
             shift = {-shift, -shift},
             scale = 32 / (child_prototype.icon_size or 64) * shadow_scale,
@@ -179,9 +203,9 @@ function py.composite_icon(base_prototype_string, child_top_left, child_top_righ
     end
 
     if child_top_right then
-        local child_prototype = data.raw.fluid[child_top_right] or data.raw.item[child_top_right] or data.raw.recipe[child_top_right]
+        local child_prototype, child_prototype_icon = get_prototype_icon(child_top_right)
         table.insert(icons, {
-            icon = child_prototype.icon,
+            icon = child_prototype_icon,
             icon_size = (child_prototype.icon_size or 64),
             shift = {shift, -shift},
             scale = 32 / (child_prototype.icon_size or 64) * shadow_scale,
@@ -190,9 +214,9 @@ function py.composite_icon(base_prototype_string, child_top_left, child_top_righ
     end
 
     if child_bottom_left then
-        local child_prototype = data.raw.fluid[child_bottom_left] or data.raw.item[child_bottom_left] or data.raw.recipe[child_bottom_left]
+        local child_prototype, child_prototype_icon = get_prototype_icon(child_bottom_left)
         table.insert(icons, {
-            icon = child_prototype.icon,
+            icon = child_prototype_icon,
             icon_size = (child_prototype.icon_size or 64),
             shift = {-shift, shift},
             scale = 32 / (child_prototype.icon_size or 64) * shadow_scale,
@@ -201,9 +225,9 @@ function py.composite_icon(base_prototype_string, child_top_left, child_top_righ
     end
 
     if child_bottom_right then
-        local child_prototype = data.raw.fluid[child_bottom_right] or data.raw.item[child_bottom_right] or data.raw.recipe[child_bottom_right]
+        local child_prototype, child_prototype_icon = get_prototype_icon(child_bottom_right)
         table.insert(icons, {
-            icon = child_prototype.icon,
+            icon = child_prototype_icon,
             icon_size = (child_prototype.icon_size or 64),
             shift = {shift, shift},
             scale = 32 / (child_prototype.icon_size or 64) * shadow_scale,
@@ -213,9 +237,9 @@ function py.composite_icon(base_prototype_string, child_top_left, child_top_righ
 
     -- Add normal children icons
     if child_top_left then
-        local child_prototype = data.raw.fluid[child_top_left] or data.raw.item[child_top_left] or data.raw.recipe[child_top_left]
+        local child_prototype, child_prototype_icon = get_prototype_icon(child_top_left)
         table.insert(icons, {
-            icon = child_prototype.icon,
+            icon = child_prototype_icon,
             icon_size = (child_prototype.icon_size or 64),
             shift = {-shift, -shift},
             scale = 32 / (child_prototype.icon_size or 64) * child_scale,
@@ -225,9 +249,9 @@ function py.composite_icon(base_prototype_string, child_top_left, child_top_righ
     end
 
     if child_top_right then
-        local child_prototype = data.raw.fluid[child_top_right] or data.raw.item[child_top_right] or data.raw.recipe[child_top_right]
+        local child_prototype, child_prototype_icon = get_prototype_icon(child_top_right)
         table.insert(icons, {
-            icon = child_prototype.icon,
+            icon = child_prototype_icon,
             icon_size = (child_prototype.icon_size or 64),
             shift = {shift, -shift},
             scale = 32 / (child_prototype.icon_size or 64) * child_scale,
@@ -237,9 +261,9 @@ function py.composite_icon(base_prototype_string, child_top_left, child_top_righ
     end
 
     if child_bottom_left then
-        local child_prototype = data.raw.fluid[child_bottom_left] or data.raw.item[child_bottom_left] or data.raw.recipe[child_bottom_left]
+        local child_prototype, child_prototype_icon = get_prototype_icon(child_bottom_left)
         table.insert(icons, {
-            icon = child_prototype.icon,
+            icon = child_prototype_icon,
             icon_size = (child_prototype.icon_size or 64),
             shift = {-shift, shift},
             scale = 32 / (child_prototype.icon_size or 64) * child_scale,
@@ -249,9 +273,9 @@ function py.composite_icon(base_prototype_string, child_top_left, child_top_righ
     end
 
     if child_bottom_right then
-        local child_prototype = data.raw.fluid[child_bottom_right] or data.raw.item[child_bottom_right] or data.raw.recipe[child_bottom_right]
+        local child_prototype, child_prototype_icon = get_prototype_icon(child_bottom_right)
         table.insert(icons, {
-            icon = child_prototype.icon,
+            icon = child_prototype_icon,
             icon_size = (child_prototype.icon_size or 64),
             shift = {shift, shift}, 
             scale = 32 / (child_prototype.icon_size or 64) * child_scale,
