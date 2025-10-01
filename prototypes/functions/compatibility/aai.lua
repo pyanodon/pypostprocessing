8f6f1ae97dcfc6770cd2582351545d6bfcf46dc6
limelou <119715684+limelou@users.noreply.github.com>
2025-10-01 09:28:32 -0700

    Add compatibility with AAI signal transmission (#139)
    * make it compatible
    
    * changelog
    
    ---------
    
    Co-authored-by: limelou <40448444+hbodas@users.noreply.github.com>

--- prototypes/functions/compatibility/aai.lua
+++ prototypes/functions/compatibility/aai.lua
@@ -7,3 +7,35 @@ if mods["aai-loaders"] then
         TECHNOLOGY("aai-fast-loader"):remove_prereq("advanced-circuit"):remove_pack("chemical-science-pack")
     end
 end
+
+if mods["aai-signal-transmission"] then
+    TECHNOLOGY("aai-signal-transmission")
+        :remove_prereq("advanced-circuit")
+        :remove_prereq("electric-engine")
+        :add_prereq("logistic-science-pack")
+        :remove_pack("chemical-science-pack")
+        :remove_pack("py-science-pack-2")
+
+    TECHNOLOGY("aai-signal-transmission").unit = {
+        count = 200,
+        time = 60,
+        ingredients = {
+            { "logistic-science-pack", 1 }
+        }
+    }
+
+    RECIPE("aai-signal-sender")
+        :replace_ingredient("processing-unit", "electronic-circuit")
+        :replace_ingredient("electric-engine-unit", "mechanical-parts-01", 2)
+        :add_ingredient({ type = "item", name = "intermetallics", amount = 20 })
+        :add_ingredient({ type = "item", name = "duralumin", amount = 20 })
+        :add_ingredient({ type = "item", name = "small-parts-01", amount = 30 })
+
+
+    RECIPE("aai-signal-receiver")
+        :replace_ingredient("processing-unit", "electronic-circuit")
+        :replace_ingredient("electric-engine-unit", "gearbox-mk01", 5)
+        :add_ingredient({ type = "item", name = "shaft-mk01", amount = 2 })
+        :add_ingredient({ type = "item", name = "lead-plate", amount = 10 })
+        :add_ingredient({ type = "item", name = "small-parts-01", amount = 10 })
+end
