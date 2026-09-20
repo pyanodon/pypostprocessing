5dba0c37b8e3c4202411b7b4c74dbdf98a8a8052
przemo1232 <p.sobczak01@gmail.com>
2026-09-20 17:25:26 +0200

    fix callsite

--- lib/log.lua
+++ lib/log.lua
@@ -2,7 +2,7 @@ py.log = {}
 
 local int_log_levels = {warning = 0, info = 1, debug = 2}
 local log_level = int_log_levels[settings.startup["pypp-log-level"].value]
-log("log level: " .. log_level)
+if helpers.stage == "prototype" then log("log level: " .. log_level) end
 
 ---@class Logger
 ---@field debug fun(message: string)
@@ -11,9 +11,11 @@ log("log level: " .. log_level)
 
 ---@param category string used to identify what the log is about
 ---@param message string
-local log_debug = function(category, message)
+---@param info_level int?
+local log_debug = function(category, message, info_level)
     if log_level < 2 then return end
-    local info = debug.getinfo(2) ---@cast info -?
+    info_level = info_level or 2
+    local info = debug.getinfo(info_level) ---@cast info -?
     local callsite = info.short_src .. ":" .. info.currentline
     
     log("DEBUG: " .. category .. ": " .. message)
@@ -22,9 +24,11 @@ end
 
 ---@param category string used to identify what the log is about
 ---@param message string
-local log_info = function(category, message)
+---@param info_level int?
+local log_info = function(category, message, info_level)
     if log_level < 1 then return end
-    local info = debug.getinfo(2) ---@cast info -?
+    info_level = info_level or 2
+    local info = debug.getinfo(info_level) ---@cast info -?
     local callsite = info.short_src .. ":" .. info.currentline
 
     log("INFO: " .. category .. ": " .. message)
@@ -33,9 +37,11 @@ end
 
 ---@param category string used to identify what the log is about
 ---@param message string
-local log_warning = function(category, message)
+---@param info_level int?
+local log_warning = function(category, message, info_level)
     if log_level < 0 then return end
-    local info = debug.getinfo(2) ---@cast info -?
+    info_level = info_level or 2
+    local info = debug.getinfo(info_level) ---@cast info -?
     local callsite = info.short_src .. ":" .. info.currentline
 
     log("WARNING: " .. category .. ": " .. message)
@@ -62,16 +68,22 @@ py.log.init = function(category)
 end
 
 ---@param message string
-py.log.debug = function(message)
-    log_debug("default", message)
+---@param category string?
+py.log.debug = function(message, category)
+    category = category or "default"
+    log_debug(category, message, 3)
 end
 
 ---@param message string
-py.log.info = function(message)
-    log_info("default", message)
+---@param category string?
+py.log.info = function(message, category)
+    category = category or "default"
+    log_info(category, message, 3)
 end
 
 ---@param message string
-py.log.warning = function(message)
-    log_warning("default", message)
+---@param category string?
+py.log.warning = function(message, category)
+    category = category or "default"
+    log_warning(category, message, 3)
 end
